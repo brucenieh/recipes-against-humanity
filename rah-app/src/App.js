d2267427f39bbd7cbc0529413a02466fb616a2fe
Kean Weng Yap <yapkeanweng1223@gmail.com>
2020-03-15 21:40:25 -0600

    react: adding fetch to get API

--- rah-app/src/App.js
+++ rah-app/src/App.js
@@ -3,17 +3,39 @@ import { Input } from "semantic-ui-react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <Input focus placeholder="Search..." />
-        </p>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ingredients: [1]
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://10.0.0.150:5000/ingredients")
+      .then(res => res.json())
+      .then((data) => {
+        this.setState({ ingredients: data.ingredients });
+      })
+      .catch(console.log);
+    console.log(this.state.ingredients);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            <Input focus placeholder="Search..." />
+            <li key={item.name}>
+              {item.name} {item.price}
+            </li>
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
